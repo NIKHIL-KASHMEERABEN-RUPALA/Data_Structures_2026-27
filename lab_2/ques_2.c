2803aca468e4481e1556499b84598697c72248f9
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-04-30 13:32:48 +0000

    ques_2_done

--- lab_2/ques_2.c
+++ lab_2/ques_2.c
@@ -1,2 +1,25 @@
 #include<stdio.h>
 
+int main(){
+
+    int power , number ;
+    printf("Enter the number :      ");
+    scanf("%d",&number);
+
+    printf("Enter the power :       ");
+    scanf("%d",&power);
+
+    int temp = power;
+
+    int final_number;
+
+    while(temp>0){
+        final_number = number*number;
+        temp--;
+    }
+
+    printf("The result is :         %d      ",final_number);
+
+
+    return 0;
+}
