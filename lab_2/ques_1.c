adf5dbf51f3f5e4679517e2eae82007f59f6ea72
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-02 15:06:09 +0000

    ques_1

--- lab_2/ques_1.c
+++ lab_2/ques_1.c
@@ -1,34 +1,62 @@
+// factorial of a number
+// #include<stdio.h>
+
+// int factorial(int number){
+//     if(number==0 || number==1){
+//         return 1;
+//     }
+//     else{
+//         return number*factorial(number-1);
+//     }
+// }
+
+// int main(){
+
+//     int n;
+//     printf("Enter the number :      ");
+//     scanf("%d",&n);
+
+//     int using_factorial = factorial(n);
+
+//     int without_factorial = 1;
+
+//     while(n>0){
+//         without_factorial*=n;
+//         n--;
+//     }
+
+//     printf("\n using factorial we get %d \n",using_factorial);
+
+//     printf("\n without factorial we get :   %d      \n",without_factorial);
+
+
+
+
+// }
+
+
+
 #include<stdio.h>
 
 int factorial(int number){
-    if(number==0 || number==1){
+    int temp = number;
+    if(temp == 0 || temp==1){
         return 1;
     }
     else{
-        return number*factorial(number-1);
+        return temp*factorial(temp-1);
     }
 }
 
 int main(){
 
-    int n;
-    printf("Enter the number :      ");
-    scanf("%d",&n);
+    int number;
+    printf("Enter a number :        ");
+    scanf("%d",&number);
 
-    int using_factorial = factorial(n);
+    int x = factorial(number);
 
-    int without_factorial = 1;
+    printf("\n %d \n",x);
 
-    while(n>0){
-        without_factorial*=n;
-        n--;
-    }
-
-    printf("\n using factorial we get %d \n",using_factorial);
-
-    printf("\n without factorial we get :   %d      \n",without_factorial);
-
-
-
-
+    return 0;
 }
--- lab_2/ques_2.c
+++ lab_2/ques_2.c
@@ -1,25 +1,54 @@
+// WAP to find power of a number using loop.
+// #include<stdio.h>
+
+// int main(){
+
+//     int power , number ;
+//     printf("Enter the number :      ");
+//     scanf("%d",&number);
+
+//     printf("Enter the power :       ");
+//     scanf("%d",&power);
+
+//     int temp = power;
+
+//     int final_number=1;
+
+//     while(temp>0){
+//         final_number*=number;
+//         temp--;
+//     }
+
+//     printf("The result is :         %d      ",final_number);
+
+
+//     return 0;
+// }
+
+
+
 #include<stdio.h>
+#include<math.h>
 
 int main(){
 
-    int power , number ;
-    printf("Enter the number :      ");
+    int base , number ;
+
+    printf("\n Enter the number :       ");
     scanf("%d",&number);
 
-    printf("Enter the power :       ");
-    scanf("%d",&power);
+    printf("\n Enter the power to do :      ");
+    scanf("%d",&base);
 
-    int temp = power;
+    int product= 1;
 
-    int final_number=1;
+    for(int i = 0 ; i < base ; i++){
 
-    while(temp>0){
-        final_number*=number;
-        temp--;
+        product *= number;
+        
     }
 
-    printf("The result is :         %d      ",final_number);
-
+    printf(" The result is %d \n",product);
 
     return 0;
 }
