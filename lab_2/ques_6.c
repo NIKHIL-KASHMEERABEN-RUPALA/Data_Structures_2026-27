2986f95efc544d21f3f449078dbee611618ab16b
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-02 14:34:52 +0000

    ques_6_done

--- lab_2/ques_6.c
+++ lab_2/ques_6.c
@@ -1,46 +1,91 @@
 // WAP to print prime numbers between given interval.
 
+// #include<stdio.h>
+// #include<stdbool.h>
+
+// bool isPrime(int number){
+
+//     for(int i = 2 ; i < number ; i++){
+//         if(number%i==0){
+//             return false;
+//         }
+//     }
+    
+//     return true;
+
+// }
+
+
+// void primeDisplay(int start, int stop){
+
+//     for(int i = start ; i <=stop ; i++){
+//         if(isPrime(i)){
+//             printf(" %d ",i);
+//         }
+//         else{
+//             continue;
+//         }
+//     }
+
+// }
+
+// int main(){
+
+//     int start , stop;
+
+//     printf("\n  Enter the starting point :      ");
+//     scanf("%d",&start);
+
+//     printf("\n Entering the stop point :        ");
+//     scanf("%d",&stop);
+
+//     primeDisplay(start,stop);
+
+
+
+//     return 0;
+// }
+
+
+
 #include<stdio.h>
 #include<stdbool.h>
 
 bool isPrime(int number){
 
-    for(int i = 2 ; i < number ; i++){
+    int flag = true;
+
+    for(int i = 2 ; i<number/2 ; i++){
         if(number%i==0){
-            return false;
+            flag=false;
+            break;
         }
     }
-    
-    return true;
+
+    return flag;
 
 }
 
 
-void primeDisplay(int start, int stop){
-
-    for(int i = start ; i <=stop ; i++){
-        if(isPrime(i)){
-            printf(" %d ",i);
-        }
-        else{
-            continue;
-        }
+void displayPrime(int start, int end){
+    for(int i = start ; i <= end ; i++){
+    if(isPrime(i)){
+        printf("\n %d \n",i);
     }
-
+}
 }
 
 int main(){
 
-    int start , stop;
+    int start , end;
 
-    printf("\n  Enter the starting point :      ");
+    printf("\n Enter the starting range :       ");
     scanf("%d",&start);
 
-    printf("\n Entering the stop point :        ");
-    scanf("%d",&stop);
+    printf("\n Enter the ending range :     ");
+    scanf("%d",&end);
 
-    primeDisplay(start,stop);
-
+    displayPrime(start,end);
 
 
     return 0;
