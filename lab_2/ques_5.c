3b150d132c772e60d0a7e7d224259b984c1cdc7f
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-02 14:38:57 +0000

    ques_5.c is done

--- lab_2/ques_5.c
+++ lab_2/ques_5.c
@@ -1,11 +1,49 @@
+// WAP to check whether a number is prime or not.
+
+// #include<stdio.h>
+// #include<stdbool.h>
+
+// bool isPrimeCheck(int number){
+
+//     bool flag = true;
+
+//     for(int i = 2; i<number ; i++){
+//         if(number%i==0){
+//             flag = false;
+//             break;
+//         }
+//     }
+
+//     return flag;
+
+
+// }
+
+// int main(){
+
+//     int n;
+//     printf("Enter the number :  ");
+//     scanf("%d",&n);
+
+//     if(isPrimeCheck(n)){
+//         printf("\n This is a prime number \n");
+//     }else{
+//         printf("\n This is not a prime number \n");
+//     }
+
+//     return 0;
+// }
+
+
+
 #include<stdio.h>
 #include<stdbool.h>
 
-bool isPrimeCheck(int number){
+
+bool isPrime(int number){
 
     bool flag = true;
-
-    for(int i = 2; i<number ; i++){
+    for(int i = 2 ; i < number/2 ; i++){
         if(number%i==0){
             flag = false;
             break;
@@ -13,21 +51,24 @@ bool isPrimeCheck(int number){
     }
 
     return flag;
-
-
 }
 
 int main(){
 
-    int n;
-    printf("Enter the number :  ");
-    scanf("%d",&n);
+    int number ;
+    printf("Enter a number to check:            ");
+    scanf("%d",&number);
 
-    if(isPrimeCheck(n)){
-        printf("\n This is a prime number \n");
-    }else{
-        printf("\n This is not a prime number \n");
+    if(isPrime(number)){
+        printf(" %d is prime ",number);
     }
+    else{
+        printf(" %d is not prime ",number);
+    }
+
+
+
+
 
     return 0;
 }
--- lab_2/ques_6.c
+++ lab_2/ques_6.c
@@ -1,92 +1,92 @@
 // WAP to print prime numbers between given interval.
 
-// #include<stdio.h>
-// #include<stdbool.h>
-
-// bool isPrime(int number){
-
-//     for(int i = 2 ; i < number ; i++){
-//         if(number%i==0){
-//             return false;
-//         }
-//     }
-    
-//     return true;
-
-// }
-
-
-// void primeDisplay(int start, int stop){
-
-//     for(int i = start ; i <=stop ; i++){
-//         if(isPrime(i)){
-//             printf(" %d ",i);
-//         }
-//         else{
-//             continue;
-//         }
-//     }
-
-// }
-
-// int main(){
-
-//     int start , stop;
-
-//     printf("\n  Enter the starting point :      ");
-//     scanf("%d",&start);
-
-//     printf("\n Entering the stop point :        ");
-//     scanf("%d",&stop);
-
-//     primeDisplay(start,stop);
-
-
-
-//     return 0;
-// }
-
-
-
 #include<stdio.h>
 #include<stdbool.h>
 
 bool isPrime(int number){
 
-    int flag = true;
-
-    for(int i = 2 ; i<number/2 ; i++){
+    for(int i = 2 ; i < number ; i++){
         if(number%i==0){
-            flag=false;
-            break;
+            return false;
+        }
+    }
+    
+    return true;
+
+}
+
+
+void primeDisplay(int start, int stop){
+
+    for(int i = start ; i <=stop ; i++){
+        if(isPrime(i)){
+            printf(" %d ",i);
+        }
+        else{
+            continue;
         }
     }
 
-    return flag;
-
-}
-
-
-void displayPrime(int start, int end){
-    for(int i = start ; i <= end ; i++){
-    if(isPrime(i)){
-        printf("\n %d \n",i);
-    }
-}
 }
 
 int main(){
 
-    int start , end;
+    int start , stop;
 
-    printf("\n Enter the starting range :       ");
+    printf("\n  Enter the starting point :      ");
     scanf("%d",&start);
 
-    printf("\n Enter the ending range :     ");
-    scanf("%d",&end);
+    printf("\n Entering the stop point :        ");
+    scanf("%d",&stop);
 
-    displayPrime(start,end);
+    primeDisplay(start,stop);
+
 
 
     return 0;
 }
+
+
+
+// #include<stdio.h>
+// #include<stdbool.h>
+
+// bool isPrime(int number){
+
+//     int flag = true;
+
+//     for(int i = 2 ; i<number/2 ; i++){
+//         if(number%i==0){
+//             flag=false;
+//             break;
+//         }
+//     }
+
+//     return flag;
+
+// }
+
+
+// void displayPrime(int start, int end){
+//     for(int i = start ; i <= end ; i++){
+//     if(isPrime(i)){
+//         printf("\n %d \n",i);
+//     }
+// }
+// }
+
+// int main(){
+
+//     int start , end;
+
+//     printf("\n Enter the starting range :       ");
+//     scanf("%d",&start);
+
+//     printf("\n Enter the ending range :     ");
+//     scanf("%d",&end);
+
+//     displayPrime(start,end);
+
+
+//     return 0;
+// }
