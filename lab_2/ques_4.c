bf6960e5e132a110e45517a8a59a8a435d90c7ab
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-02 14:43:44 +0000

    ques_4_done

--- lab_2/ques_4.c
+++ lab_2/ques_4.c
@@ -1,29 +1,72 @@
-#include<stdio.h>
+// WAP to find factors of a given number.
 
-void findFactors(int number){
+// #include<stdio.h>
 
-    for(int i = 1 ; i <=number ; i++)
-    {
-        if(number%i==0){
-            printf(" %d  ",i);
-        }else{
-            continue;
-        }
+// void findFactors(int number){
+
+//     for(int i = 1 ; i <=number ; i++)
+//     {
+//         if(number%i==0){
+//             printf(" %d  ",i);
+//         }else{
+//             continue;
+//         }
         
+//     }
+
+// }
+
+// int main(){
+
+//     int number ;
+
+//     printf("\n  Enter the number :     ");
+//     scanf("%d",&number);
+
+//     findFactors(number);
+
+
+//     return 0;
+
+// }
+
+
+#include<stdio.h>
+#include<stdbool.h>
+
+
+bool isFactor(int i,int number){
+
+    bool flag = true;
+    if(number%i!=0){
+        flag = false;
     }
+
+    return flag;
 
 }
 
+void display_factors(int number){
+    for(int i = 1 ; i<=number ; i++){
+        if(isFactor(i,number)){
+            printf(" %d is a factor \n",i);
+        }
+        else{
+            continue;
+        }
+    }
+}
+
+
 int main(){
 
-    int number ;
-
-    printf("\n  Enter the number :     ");
+    int number;
+    printf("Enter a number :        ");
     scanf("%d",&number);
 
-    findFactors(number);
+    display_factors(number);
+
 
 
     return 0;
-
 }
--- lab_2/ques_5.c
+++ lab_2/ques_5.c
@@ -1,49 +1,13 @@
 // WAP to check whether a number is prime or not.
 
-// #include<stdio.h>
-// #include<stdbool.h>
-
-// bool isPrimeCheck(int number){
-
-//     bool flag = true;
-
-//     for(int i = 2; i<number ; i++){
-//         if(number%i==0){
-//             flag = false;
-//             break;
-//         }
-//     }
-
-//     return flag;
-
-
-// }
-
-// int main(){
-
-//     int n;
-//     printf("Enter the number :  ");
-//     scanf("%d",&n);
-
-//     if(isPrimeCheck(n)){
-//         printf("\n This is a prime number \n");
-//     }else{
-//         printf("\n This is not a prime number \n");
-//     }
-
-//     return 0;
-// }
-
-
-
 #include<stdio.h>
 #include<stdbool.h>
 
-
-bool isPrime(int number){
+bool isPrimeCheck(int number){
 
     bool flag = true;
-    for(int i = 2 ; i < number/2 ; i++){
+
+    for(int i = 2; i<number ; i++){
         if(number%i==0){
             flag = false;
             break;
@@ -51,24 +15,60 @@ bool isPrime(int number){
     }
 
     return flag;
+
+
 }
 
 int main(){
 
-    int number ;
-    printf("Enter a number to check:            ");
-    scanf("%d",&number);
+    int n;
+    printf("Enter the number :  ");
+    scanf("%d",&n);
 
-    if(isPrime(number)){
-        printf(" %d is prime ",number);
+    if(isPrimeCheck(n)){
+        printf("\n This is a prime number \n");
+    }else{
+        printf("\n This is not a prime number \n");
     }
-    else{
-        printf(" %d is not prime ",number);
-    }
-
-
-
-
 
     return 0;
 }
+
+
+
+// #include<stdio.h>
+// #include<stdbool.h>
+
+
+// bool isPrime(int number){
+
+//     bool flag = true;
+//     for(int i = 2 ; i < number/2 ; i++){
+//         if(number%i==0){
+//             flag = false;
+//             break;
+//         }
+//     }
+
+//     return flag;
+// }
+
+// int main(){
+
+//     int number ;
+//     printf("Enter a number to check:            ");
+//     scanf("%d",&number);
+
+//     if(isPrime(number)){
+//         printf(" %d is prime ",number);
+//     }
+//     else{
+//         printf(" %d is not prime ",number);
+//     }
+
+
+
+
+
+//     return 0;
+// }
