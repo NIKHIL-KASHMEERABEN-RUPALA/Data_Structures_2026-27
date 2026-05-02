ced9e34bb242936fd9b766e554bdb39686082624
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-02 14:27:45 +0000

    ques_8_done

--- lab_2/ques_7.c
+++ lab_2/ques_7.c
@@ -1,20 +1,46 @@
+// WAP to find the sum of 1 + (1+2) + (1+2+3) + (1+2+3+4)+ ...+(1+2+3+4+....+n).
+// #include<stdio.h>
+
+// int main(){
+
+//     int n;
+//     printf("\n  Enter the value of n:       ");
+//     scanf("%d",&n);
+
+//     int sum = 0 ;
+
+//     for(int i = 1 ; i <= n ; i++){
+//         for(int j = i ; j>0 ; j--){
+//             sum+=j;
+//         }
+//     }
+
+//     printf("%d \n",sum);
+
+
+//     return 0;
+// }
+
+
 #include<stdio.h>
 
 int main(){
 
-    int n;
-    printf("\n  Enter the value of n:       ");
-    scanf("%d",&n);
+    int user_input;
+
+    printf("Enter a valid input ;           ");
+    scanf("%d",&user_input);
 
     int sum = 0 ;
 
-    for(int i = 1 ; i <= n ; i++){
-        for(int j = i ; j>0 ; j--){
-            sum+=j;
+    for(int i = 1 ; i <= user_input ; i++){
+        int j = i;
+        while(j>0){
+            sum += j;
+            j--;
         }
+        
     }
-
-    printf("%d \n",sum);
 
 
     return 0;
--- lab_2/ques_8.c
+++ lab_2/ques_8.c
@@ -1,46 +1,99 @@
-// WAP to print Armstrong number from 1 to 1000.
+// // WAP to print Armstrong number from 1 to 1000.
+// #include<stdio.h>
+// #include<stdbool.h>
+// #include<string.h>
+// #include<math.h>
+
+
+// bool isArmstrong(int number){
+
+//     int count = 0;
+
+//     int temp = number;
+
+//     while(temp!=0){
+//         count++;
+//         temp=temp/10;
+//     }
+
+
+//     int digit;
+
+//     temp = number;
+//     int product_sum = 0;
+
+//     while(temp>0){
+//         digit = temp%10;
+//         product_sum += pow(digit,count);
+//         temp /= 10;
+//     }
+
+//     return (product_sum==number);
+
+// }
+
+// int main(){
+
+//     int start,end;
+//     printf("Enter the starting point :      ");
+//     scanf("%d",&start);
+
+//     printf("Enter the ending point :        ");
+//     scanf("%d",&end);
+
+//     for(int i = start ; i <= end ; i++ ){
+//         if(isArmstrong(i)){
+//             printf("%d",i);
+//         }
+//         else{
+//             continue;
+//         }
+//     }
+
+//     return 0;
+// }
+
+
+
+
 #include<stdio.h>
 #include<stdbool.h>
-#include<string.h>
 #include<math.h>
-
 
 bool isArmstrong(int number){
 
-    char x = number;
-
-    int length = strlen(x);
-
-    int digit;
-
+    int total_digits = 0 ;
     int temp = number;
-    int product_sum = 0;
 
     while(temp>0){
-        digit = number%10;
-        product_sum += pow(digit,length);
+        total_digits += 1;
+        temp = temp/10;
     }
 
-    return (product_sum==number);
+    int sum_product = 0;
 
+    temp = number;
+    while(temp>0){
+        int digit = temp%10;
+        sum_product += pow(digit,total_digits);
+        temp = temp/10;
+    }
+
+    return (sum_product==number);
 }
+
 
 int main(){
 
-    int start,end;
-    printf("Enter the starting point :      ");
-    scanf("%d",&start);
+   
+    for(int i = 1 ; i<=1000 ; i++){
 
-    printf("Enter the ending point :        ");
-    scanf("%d",&end);
-
-    for(int i = start ; i <= end ; i++ ){
-        if(isArmstrong(i)){
-            printf("%d",i);
-        }
-        else{
-            continue;
-        }
+    if(isArmstrong(i)){
+        printf("\n %d is amrstrong number \n",i);
+    }
+    else{
+        continue;
+    }
     }
 
     return 0;
