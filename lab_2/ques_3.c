2e5a757695905833c239c17a0fe16d2038321c55
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-02 14:50:20 +0000

    ques_3_done

--- lab_2/ques_3.c
+++ lab_2/ques_3.c
@@ -1,22 +1,59 @@
+// WAP to reverse a number.
+// #include<stdio.h>
+
+// int main(){
+
+//     int number;
+//     printf("Enter the number :      ");
+//     scanf("%d",&number);
+
+//     int reverse_number;
+    
+
+//     while(number>0){
+//         int digit = 1;
+//         digit = number%10;
+//         reverse_number = reverse_number*10+digit;
+//         number = number/10;
+//     }
+
+//     printf("The reversed number is :    %d  ",reverse_number);
+
+//     return 0;
+// }
+
+
 #include<stdio.h>
+#include<stdbool.h>
+
+
+int palindromeConverter(int number){
+    int reverse = 0;
+    int temp = number;
+    
+    while(temp>0){
+        int digit = temp%10;
+        reverse = reverse*10+digit;
+        temp = temp/10;
+    }
+
+    return(reverse);
+}
 
 int main(){
 
-    int number;
-    printf("Enter the number :      ");
+
+    int number ;
+    
+    printf("Enter a number to reverse ;           ");
     scanf("%d",&number);
 
-    int reverse_number;
+    int result = palindromeConverter(number);
+
+    printf(" %d is the reversed number ",result);
+
+
     
-
-    while(number>0){
-        int digit = 1;
-        digit = number%10;
-        reverse_number = reverse_number*10+digit;
-        number = number/10;
-    }
-
-    printf("The reversed number is :    %d  ",reverse_number);
 
     return 0;
 }
