0beeb2c993eafb03ebd54b68a0ff9b0310072305
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-18 12:42:06 +0000

    changes in ques_3

--- lab_1/ques_3.c
+++ lab_1/ques_3.c
@@ -1,31 +1,46 @@
 #include<stdio.h>
-#include<ctype.h> // to use the tolower() fucntion
+#include<ctype.h>
 
 int main(){
 
-    char x;
-    printf("Enter the character to check :          ");
-    scanf("%c",&x);
+    char new , original;
 
-    char hey = tolower(x);
+    printf("\n Enter a character :      ");
+    scanf("%c",&original);
 
-    switch(hey){
+    new = tolower(original);
+
+    switch(original){
         case 'a':
+        printf("\n This is a vowel ");
+        break;
+
         case 'e':
+        printf("\n This is a vowel ");
+        break;
+
+
         case 'i':
+        printf("\n This is a vowel  ");
+        break;
+
+
         case 'o':
+        printf("\n This is a vowel ");
+        break;
+
+
         case 'u':
-            printf("\n This is a vowel ! \n");
-            break;
+        printf("\n This is a vowel : u ");
+        break;
 
         default:
-            if(x>'a' && x<'z' || x>'A' && x<'Z'){
-                printf("\n This is a consonant ! \n");
-            }
-            else{
-                printf("\n This is a number \n");
-            }
+        printf("\n Entered character is not vowel ");
+        break;
     }
+
+    
+
 
 
     return 0;
