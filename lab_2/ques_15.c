bbd51ccc878e83121112945baf27850196958954
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-19 14:51:58 +0000

    done ques_15

--- lab_2/ques_15.c
+++ lab_2/ques_15.c
@@ -1,99 +1,40 @@
-// // WAP to print Armstrong number from 1 to 1000.
-// #include<stdio.h>
-// #include<stdbool.h>
-// #include<string.h>
-// #include<math.h>
-
-
-// bool isArmstrong(int number){
-
-//     int count = 0;
-
-//     int temp = number;
-
-//     while(temp!=0){
-//         count++;
-//         temp=temp/10;
-//     }
-
-
-//     int digit;
-
-//     temp = number;
-//     int product_sum = 0;
-
-//     while(temp>0){
-//         digit = temp%10;
-//         product_sum += pow(digit,count);
-//         temp /= 10;
-//     }
-
-//     return (product_sum==number);
-
-// }
-
-// int main(){
-
-//     int start,end;
-//     printf("Enter the starting point :      ");
-//     scanf("%d",&start);
-
-//     printf("Enter the ending point :        ");
-//     scanf("%d",&end);
-
-//     for(int i = start ; i <= end ; i++ ){
-//         if(isArmstrong(i)){
-//             printf("%d",i);
-//         }
-//         else{
-//             continue;
-//         }
-//     }
-
-//     return 0;
-// }
-
-
-
-
 #include<stdio.h>
-#include<stdbool.h>
 #include<math.h>
+#include<stdbool.h>
+
 
 bool isArmstrong(int number){
+    int digits = 0, sum_product = 0;
 
-    int total_digits = 0 ;
-    int temp = number;
-
-    while(temp>0){
-        total_digits += 1;
-        temp = temp/10;
+    for(int i = number ; i>0 ; i/=10){
+        digits++;
     }
 
-    int sum_product = 0;
-
-    temp = number;
-    while(temp>0){
-        int digit = temp%10;
-        sum_product += pow(digit,total_digits);
-        temp = temp/10;
+    int digit =0;
+    for(int j = number ; j>0 ; j/=10){
+        digit = j%10;
+        sum_product += pow(digit,digits);
     }
 
-    return (sum_product==number);
+
+    return(sum_product==number);
+
 }
 
 
 int main(){
 
-   
-    for(int i = 1 ; i<=1000 ; i++){
+    int number;
 
-    if(isArmstrong(i)){
-        printf("\n %d is amrstrong number \n",i);
-    }
-    else{
-        continue;
-    }
+
+    for(int i = 1 ; i<=1000 ; i++){
+        
+        if(isArmstrong(i)){
+            printf("\n %d",i);
+        }else{
+            continue;
+        }
+
     }
 
     return 0;
