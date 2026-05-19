aad3d36e619abb035c7d64497f5926247ab27349
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-19 13:27:08 +0000

    changes in ques_14

--- lab_2/ques_14.c
+++ lab_2/ques_14.c
@@ -1,49 +1,19 @@
-// WAP to find the sum of 1 + (1+2) + (1+2+3) + (1+2+3+4)+ ...+(1+2+3+4+....+n).
-// #include<stdio.h>
-
-// int main(){
-
-//     int n;
-//     printf("\n  Enter the value of n:       ");
-//     scanf("%d",&n);
-
-//     int sum = 0 ;
-
-//     for(int i = 1 ; i <= n ; i++){
-//         for(int j = i ; j>0 ; j--){
-//             sum+=j;
-//         }
-//     }
-
-//     printf("%d \n",sum);
-
-
-//     return 0;
-// }
-
-
 #include<stdio.h>
-
 int main(){
+    int n;
 
-    int user_input;
+    printf("Enter the value of n :      ");
+    scanf("%d",&n);
+    int sum = 0;
 
-    printf("Enter a valid input ;           ");
-    scanf("%d",&user_input);
 
-    int sum = 0 ;
-
-    for(int i = 1 ; i <= user_input ; i++){
-        int j = i;
-        while(j>0){
-            sum += j;
-            j--;
+    for(int i = 1 ; i<=n ; i++){
+        for(int j=1 ; j<=i ; j++){
+            sum+=j;
         }
-
     }
 
-    printf("%d \n",sum);
-
+    printf("The total sum of the series is: %d\n", sum);
 
     return 0;
 }
