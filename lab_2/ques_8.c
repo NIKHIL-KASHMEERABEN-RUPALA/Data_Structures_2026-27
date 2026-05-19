b46b21c2c9687314991e254ae0c6d4bddeef234c
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-19 12:12:35 +0000

    changes in ques_8

--- lab_2/ques_8.c
+++ lab_2/ques_8.c
@@ -1,62 +1,35 @@
-// factorial of a number
-// #include<stdio.h>
-
-// int factorial(int number){
-//     if(number==0 || number==1){
-//         return 1;
-//     }
-//     else{
-//         return number*factorial(number-1);
-//     }
-// }
-
-// int main(){
-
-//     int n;
-//     printf("Enter the number :      ");
-//     scanf("%d",&n);
-
-//     int using_factorial = factorial(n);
-
-//     int without_factorial = 1;
-
-//     while(n>0){
-//         without_factorial*=n;
-//         n--;
-//     }
-
-//     printf("\n using factorial we get %d \n",using_factorial);
-
-//     printf("\n without factorial we get :   %d      \n",without_factorial);
-
-
-
-
-// }
-
-
-
 #include<stdio.h>
 
+
+
+
 int factorial(int number){
-    int temp = number;
-    if(temp == 0 || temp==1){
+    if(number==0 || number==1){
         return 1;
     }
-    else{
-        return temp*factorial(temp-1);
-    }
+    return number*factorial(number-1);
 }
+
 
 int main(){
 
-    int number;
-    printf("Enter a number :        ");
-    scanf("%d",&number);
 
-    int x = factorial(number);
+    int input;
+    printf("\n Enter the number :       ");
+    scanf("%d",&input);
 
-    printf("\n %d \n",x);
+    int result = factorial(input);
+
+    printf("\n The factorial using recurssion is :   %d    ",result);
+
+    int using_loop=1;
+    for(int i = input ; i>0 ; i--){
+        using_loop*=i;
+    }
+
+    printf("\n The factorial using loop is :    %d      ",using_loop);
+
+
 
     return 0;
 }
