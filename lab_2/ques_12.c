11ea2d6a789fda78ca30cdba7f0b79a2be19ee76
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-19 12:36:04 +0000

    changes in ques_12

--- lab_2/ques_12.c
+++ lab_2/ques_12.c
@@ -1,74 +1,28 @@
-// WAP to check whether a number is prime or not.
-
 #include<stdio.h>
 #include<stdbool.h>
 
-bool isPrimeCheck(int number){
+int main(){
 
+    int number;
+
+    printf("\n Enter the number to check :          ");
+    scanf("%d",&number);
     bool flag = true;
-
-    for(int i = 2; i<number ; i++){
+    for(int i = 2 ; i<=number/2; i++){
+        
         if(number%i==0){
+            printf("\n The number is not prime");
             flag = false;
             break;
+        }else{
+            continue;
         }
     }
 
-    return flag;
-
-
-}
-
-int main(){
-
-    int n;
-    printf("Enter the number :  ");
-    scanf("%d",&n);
-
-    if(isPrimeCheck(n)){
-        printf("\n This is a prime number \n");
-    }else{
-        printf("\n This is not a prime number \n");
+    if(flag){
+        printf("\n The number is prime");
     }
+
 
     return 0;
 }
-
-
-
-// #include<stdio.h>
-// #include<stdbool.h>
-
-
-// bool isPrime(int number){
-
-//     bool flag = true;
-//     for(int i = 2 ; i < number/2 ; i++){
-//         if(number%i==0){
-//             flag = false;
-//             break;
-//         }
-//     }
-
-//     return flag;
-// }
-
-// int main(){
-
-//     int number ;
-//     printf("Enter a number to check:            ");
-//     scanf("%d",&number);
-
-//     if(isPrime(number)){
-//         printf(" %d is prime ",number);
-//     }
-//     else{
-//         printf(" %d is not prime ",number);
-//     }
-
-
-
-
-
-//     return 0;
-// }
