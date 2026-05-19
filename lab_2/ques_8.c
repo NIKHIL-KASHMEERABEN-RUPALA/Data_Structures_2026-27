6f9e78f3bc6b401eaf410de298d2a5d11338baf5
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-19 12:05:48 +0000

    many changes

--- lab_2/ques_8.c
+++ lab_2/ques_8.c
@@ -1,100 +1,62 @@
-// // WAP to print Armstrong number from 1 to 1000.
+// factorial of a number
 // #include<stdio.h>
-// #include<stdbool.h>
-// #include<string.h>
-// #include<math.h>
 
-
-// bool isArmstrong(int number){
-
-//     int count = 0;
-
-//     int temp = number;
-
-//     while(temp!=0){
-//         count++;
-//         temp=temp/10;
+// int factorial(int number){
+//     if(number==0 || number==1){
+//         return 1;
 //     }
-
-
-//     int digit;
-
-//     temp = number;
-//     int product_sum = 0;
-
-//     while(temp>0){
-//         digit = temp%10;
-//         product_sum += pow(digit,count);
-//         temp /= 10;
+//     else{
+//         return number*factorial(number-1);
 //     }
-
-//     return (product_sum==number);
-
 // }
 
 // int main(){
 
-//     int start,end;
-//     printf("Enter the starting point :      ");
-//     scanf("%d",&start);
+//     int n;
+//     printf("Enter the number :      ");
+//     scanf("%d",&n);
 
-//     printf("Enter the ending point :        ");
-//     scanf("%d",&end);
+//     int using_factorial = factorial(n);
 
-//     for(int i = start ; i <= end ; i++ ){
-//         if(isArmstrong(i)){
-//             printf("%d",i);
-//         }
-//         else{
-//             continue;
-//         }
+//     int without_factorial = 1;
+
+//     while(n>0){
+//         without_factorial*=n;
+//         n--;
 //     }
 
-//     return 0;
+//     printf("\n using factorial we get %d \n",using_factorial);
+
+//     printf("\n without factorial we get :   %d      \n",without_factorial);
+
+
+
+
 // }
 
 
 
-
 #include<stdio.h>
-#include<stdbool.h>
-#include<math.h>
 
-bool isArmstrong(int number){
-
-    int total_digits = 0 ;
+int factorial(int number){
     int temp = number;
-
-    while(temp>0){
-        total_digits += 1;
-        temp = temp/10;
+    if(temp == 0 || temp==1){
+        return 1;
     }
-
-    int sum_product = 0;
-
-    temp = number;
-    while(temp>0){
-        int digit = temp%10;
-        sum_product += pow(digit,total_digits);
-        temp = temp/10;
+    else{
+        return temp*factorial(temp-1);
     }
-
-    return (sum_product==number);
 }
-
 
 int main(){
 
-   
-    for(int i = 1 ; i<=1000 ; i++){
+    int number;
+    printf("Enter a number :        ");
+    scanf("%d",&number);
 
-    if(isArmstrong(i)){
-        printf("\n %d is amrstrong number \n",i);
-    }
-    else{
-        continue;
-    }
-    }
+    int x = factorial(number);
+
+    printf("\n %d \n",x);
 
     return 0;
 }
