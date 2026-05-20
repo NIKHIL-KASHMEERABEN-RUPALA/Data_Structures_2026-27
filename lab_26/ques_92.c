5162fe95dccbd2ca1049706ab66c190458005537
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-20 03:01:32 +0000

    minute_changes

---
+++ lab_26/ques_92.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+// bubble sort ---> best case is O(n)
+// worst case is O(n^2)
+
+// we can use optimization that if no element is swapped then it means that 
+// array is already sorted and no swapping has taken place 
+
+void bubbleSort(int arr[]){
+    
+}
