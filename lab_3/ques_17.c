08b00afc2b992d0a90a3f845b426464a211756e2
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-20 14:48:02 +0000

    done ques_17.c

--- lab_3/ques_17.c
+++ lab_3/ques_17.c
@@ -1,22 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main() {
+    int m, n, sum = 0;
 
-    int m,n,sum=0;
+    printf("Enter the value of m: ");
+    scanf("%d", &m);
 
-    printf("\n Enter the value of m:        ");
-    scanf("%d",&m);
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
 
-    printf("\n Enter the value of n:        ");
-    scanf("%d",&n);
-
-
-    for(int i = m ; i<=n ; i++){
-        sum+=i;
+    // Edge Case Protection: If m is greater than n, swap them!
+    if (m > n) {
+        int temp = m;
+        m = n;
+        n = temp;
     }
 
-    printf("\n The result is :    %d      ",sum);
+    for(int i = m; i <= n; i++) {
+        sum += i;
+    }
 
+    printf("\nThe sum of numbers from %d to %d is: %d\n", m, n, sum);
 
     return 0;
 }
