3a8dfd638d33aea6ff86d265c88137e9c271c152
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-22 16:31:00 +0000

    done ques_34.c

--- lab_6/ques_34.c
+++ lab_6/ques_34.c
@@ -5,7 +5,7 @@ int stack[size];
 int top = -1;
 
 
-void push(int element){
+void push(){
     if(top == size-1){
         printf("\n Stack overflow !");
         return;
@@ -64,6 +64,30 @@ int peep(){
 
     if(position<=0 || position>top+1){
         printf("\n Invalid position");
+    }else{
+        printf(" %d was at the top ",stack[top-position+1]);
+    }
+
+}
+
+
+void change(){
+
+    int position , new_element;
+    printf("\n Enter the postition from top :       ");
+    scanf("%d",&position);
+
+    if(top==-1){
+        printf("\n The stack is empty");
+        return;
+    }
+    if(position<=0 || position>top+1){
+        printf("\n Invalid position ");
+    }else{
+        printf("\n Enter the new element to insert :        ");
+        scanf("%d",&new_element);
+        stack[size-position+1] = new_element;
+        printf("\n The new element was inserted successfully !  ");
     }
 
 }
@@ -72,6 +96,54 @@ int peep(){
 int main(){
 
 
+    int choice;
+
+    while(1){
+        printf("\n Enter the choice from following :        ");
+        
+        printf("\n 1. Push ");
+        printf("\n 2. Pop ");
+        printf("\n 3. Display ");
+        printf("\n 4. Peep ");
+        printf("\n 5. Change ");
+        printf("\n 6. Exit");
+
+        scanf("%d",&choice);
+
+
+        switch(choice){
+            case 1:
+                push();
+                break;
+            
+            case 2:
+                pop();
+                break;
+            
+            case 3:
+                display();
+                break;
+
+            case 4:
+                peep();
+                break;
+
+            case 5:
+                change();
+                break;
+
+            case 6:
+                return 0;
+                break;
+
+            default:
+                printf("\n Enter a valid choice ! ");
+                break;
+
+        }
+        
+        
+    }
 
 
     return 0;
