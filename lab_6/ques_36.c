4f3401324278185e8e74aa06a5e066d348b2ce35
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-24 02:33:12 +0000

    done part of ques_36

---
+++ lab_6/ques_36.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#define size 20
+#include<string.h>
+
+char stack[size];
+int top = -1;
+
+
+
+
+void push(char element){
+    if(top==-1){
+        printf("\n Stack overflow condition !");
+        return;
+    }
+    else{
+        stack[++top] = element;
+    }
+}
+
+char pop(){
+    if(top<0){
+        printf("\n Stack underflow condition");
+        return '\0';
+    }else{
+        char popped = stack[top--] ;
+        return popped;
+    }
+}
+
+
+void quesLogic(){
+    char input[size];
+    printf("\n Enter the string :       ");
+    scanf("%s",&input);
+
+    int count_a = 1 , count_b = 1 , i=0;
+
+    while(input[i]!='b' && input[i]!='\0'){
+        count_a++;
+        push(input[i]);
+        i++;
+    }
+    if(input[i]=='\0'){
+        printf("\n Invaliid pattern entered ");
+        return;
+    }
+
+    while(input[i]!='\0'){
+        char to_check = pop();
+
+    }
+    
+}
