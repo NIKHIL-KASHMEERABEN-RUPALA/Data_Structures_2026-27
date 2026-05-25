cc01f70d12a821fdd3a222507120512bfd244337
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-25 17:13:05 +0000

    done ques_46 properly

--- lab_10/ques_46.c
+++ lab_10/ques_46.c
@@ -1,116 +1,112 @@
-#include <stdio.h>
+#include<stdio.h>
+#define size 20
 
-#define SIZE 5
-
-int queue[SIZE];
+int queue[size];
 int front = -1;
 int rear = -1;
 
-// Function to insert element
-void enqueue(int value)
-{
-    if (rear == SIZE - 1)
-    {
-        printf("\nQueue Overflow! Queue is full.\n");
-    }
-    else
-    {
-        // First element insertion
-        if (front == -1)
-        {
-            front = 0;
-        }
 
+
+void enqueue(int value){
+
+    if(rear==size-1){
+        printf("\n Queue overflow condition ! ");
+    }
+
+    else{
+
+
+        if(front==-1){
+            front = 0 ;
+        }
         rear++;
         queue[rear] = value;
 
-        printf("\n%d inserted successfully.\n", value);
-    }
-}
+        printf("\n Inserted %d successfully ! ",value);
 
-// Function to delete element
-void dequeue()
-{
-    if (front == -1 || front > rear)
-    {
-        printf("\nQueue Underflow! Queue is empty.\n");
-    }
-    else
-    {
-        printf("\nDeleted element: %d\n", queue[front]);
-        front++;
-
-        // Reset queue when empty
-        if (front > rear)
-        {
-            front = rear = -1;
         }
-    }
+
 }
 
-// Function to display queue
-void display()
-{
-    int i;
 
-    if (front == -1)
-    {
-        printf("\nQueue is empty.\n");
+
+void dequeue(){
+    if(front==-1 || front>rear){
+        printf("\n queue underflow ! ");
     }
-    else
-    {
+    else{
+        int removed = queue[front];
+        printf("\n Removed %d successfully ! ",removed);
+        front++;
+    
+
+        if(front>rear){
+            front = -1;
+            rear = -1;
+        }
+
+        }
+    
+}
+
+
+
+void display(){
+    if(front==-1 || front>rear){
+        printf("\n Queue is already empty !");
+    }
+    else{
         printf("\nQueue elements are:\n");
 
-        for (i = front; i <= rear; i++)
-        {
-            printf("%d ", queue[i]);
+        for(int i = front ; i <= rear ; i++){
+            printf(" %d ",queue[i]);
         }
-
         printf("\n");
     }
 }
 
-int main()
-{
-    int choice, value;
 
-    do
-    {
-        printf("\n===== QUEUE MENU =====\n");
-        printf("1. ENQUEUE\n");
-        printf("2. DEQUEUE\n");
-        printf("3. DISPLAY\n");
-        printf("4. EXIT\n");
+int main(){
 
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
 
-        switch (choice)
-        {
+    int choice , value;
+
+    do{
+        printf("\n Enter a choice from following ------- ");
+        printf("\n 1. Enqueue ");
+        printf("\n 2. Dequeue ");
+        printf("\n 3. Display ");
+        printf("\n 4. Exit");
+        scanf("%d",&choice);
+
+        switch(choice){
             case 1:
-                printf("Enter value to insert: ");
-                scanf("%d", &value);
-
+                printf("\n Enter the value to insert :      ");
+                scanf("%d",&value);
                 enqueue(value);
                 break;
-
+            
             case 2:
                 dequeue();
                 break;
-
+            
             case 3:
                 display();
                 break;
 
             case 4:
-                printf("\nProgram exited.\n");
+                printf("\n Program exited");
                 break;
 
             default:
-                printf("\nInvalid choice!\n");
-        }
+                printf("\n Invalid choice");
 
-    } while (choice != 4);
+        }
+        
+
+    } while(choice!=4);
+
+
 
     return 0;
 }
