8fe386300208bb3051ee43b3a5ed8fd4547a431d
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-27 04:40:39 +0000

    completed ques_49

--- lab_11/ques_49.c
+++ lab_11/ques_49.c
@@ -3,49 +3,37 @@
 
 int priority_queue[size];
 int priority[size];
-int front = -1;
 int rear = -1;
 
 
 void insert(){
-    if(rear==size-1){
-        printf("\n Underflow !");
+    if(rear == size - 1){
+        printf("\n overflow !");
         return;
     }
     rear++;
 
-    int i;
-    
-    int element , its_prio;
+    int element, its_prio;
     printf("\n Enter element to insert :        ");
-    scanf("%d",&element);
+    scanf("%d", &element);
 
     printf("\n Enter its priority:  ");
-    scanf("%d",&its_prio);
+    scanf("%d", &its_prio);
 
-    for(i = rear ; i > 0  ; i--){
-
-        if(its_prio > priority[i-1]){
-            int temp = priority[i-1];
-            priority[i-1] = its_prio;
-            priority[i] = temp;
-
-            temp = priority_queue[i-1];
-            priority_queue[i-1] = element;
-            priority_queue[i] = temp;
-
-        }
-        else{
-            break;
-        }
-
+    
+    int i = rear;
+    while(i > 0 && priority[i - 1] < its_prio) {
+        priority_queue[i] = priority_queue[i - 1];
+        priority[i] = priority[i - 1];
+        i--;
     }
 
+    
     priority_queue[i] = element;
     priority[i] = its_prio;
     
+    printf("\n Element inserted successfully.");
 }
-
 
 
 void deleteElement(){
@@ -55,10 +43,11 @@ void deleteElement(){
 
     if(rear==-1){
         printf("\n Underflow ! ");
+        return;
     }
 
     printf("\n The deleted element : %d , priority is : %d ",priority_queue[0],priority[0]);
-    for(int i = 0 ; i <=rear ; i++){
+    for(int i = 0 ; i <rear ; i++){
         priority[i] = priority[i+1];
         priority_queue[i] = priority_queue[i+1];
     }
@@ -69,14 +58,58 @@ void deleteElement(){
 
 
 void display(){
-    
+    if(rear==-1){
+        printf("\n Queue is already empty !");
+        return;
+    }
+
+    printf("\n ELements in priority queue are -------- ");
+
+    printf("\n Element \t priority \n");
+
+    for(int i = 0 ; i <=rear ; i++){
+        printf("\n %d \t %d ",priority_queue[i],priority[i]);
+    }
 }
 
 
 
 int main(){
 
+    int choice;
 
+    do{
+        printf("\n Enter a choice :     ");
+        printf("\n 1. Enqueue");
+        printf("\n 2. Dequeue");
+        printf("\n 3. Display");
+        printf("\n 4. Exit the programme");
+        scanf("%d",&choice);
+
+        switch(choice){
+            case 1:
+                insert();
+                break;
+            
+            case 2:
+                deleteElement();
+                break;
+            
+            case 3:
+                display();
+                break;
+            
+            case 4:
+                printf("\n The programme has been terminated ! ");
+                break;
+            
+            default:
+                printf("\n Invalid choice !");
+
+        }
+
+
+    }while(choice != 4);
 
 
     return 0;
