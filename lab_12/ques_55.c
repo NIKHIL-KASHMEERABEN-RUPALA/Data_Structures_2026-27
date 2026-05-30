f5f121f9ddb7298f8f387bf6eebf2bc0272af885
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-05-30 13:05:30 +0000

    ques_55 done

--- lab_12/ques_55.c
+++ lab_12/ques_55.c
@@ -1,48 +1,34 @@
-// WAP to check whether the string is Palindrome or not using Pointer.
+#include <stdio.h>
+#include <string.h>
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<stdbool.h>
+int main()
+{
+    char str[100];
+    char *start, *end;
+    int palindrome = 1;
 
-bool isPalindrome(char *arr , int n){
-    bool flag = true;
-    for(int i = 0 ; i <n/2 ; i++){
-        for(int j = n ; j>=n/2 ; j--){
-            if(*(arr+j)!=*(arr+i)){
-                flag = false;
-            }
-            else{
-                break;
-                continue;
-            }
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    start = str;
+    end = str + strlen(str) - 1;
+
+    while(start < end)
+    {
+        if(*start != *end)
+        {
+            palindrome = 0;
+            break;
         }
+
+        start++;
+        end--;
     }
 
-    return flag;
-}
-
-
-
-int main(){
-
-    char arr[100];
-
-    printf("\n Enter the elements of string to check :     \n");
-    scanf("%s",arr);
-
-    int n = strlen(arr);
-
-    bool result = isPalindrome(arr,n);
-
-    if(result){
-        printf("This is palindrome stirng !");
-    }else{
-        printf("Not a palindrome string ");
-    }
-
-
-
+    if(palindrome)
+        printf("Palindrome String");
+    else
+        printf("Not a Palindrome String");
 
     return 0;
 }
