e3e9cd30e4ca895ac49f25debe732b8466a6c6b7
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-07-18 13:47:16 +0000

    changes in menu.c

---
+++ stack/menu.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#define max 10
+int stack[max];
+int top = -1;
+
+void push(int element){
+    if(top==max-1){
+        printf("Stack overflow ! ");
+        return;
+    }
+    stack[++top] = element;
+}
+
+void pop(){
+    if(top==-1){
+        printf("\n Stack underflow ! ");
+        return;
+    }
+    printf("%d is the popped element ",stack[top--]);
+}
+
+void display(){
+    if(top==-1){
+        printf("\n THe entire stack is empty !");
+        return;
+    }
+    
+    for(int i = top ; i>=0 ; i--){
+        printf(" %d ",stack[i]);
+    }
+}
+
+void peep(){
+    int position;
+    printf("\n Enter the position to be peeped from top :     ");
+    scanf("%d",&position);
+
+    int index = top - position + 1;
+    if(index<0 || index > top){
+        printf("\n Invalid position")
+    }
+}
