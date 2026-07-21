78b0b11c9f40a70a743d8c40fc1efa6af73dc382
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-07-21 03:39:45 +0000

    certain changes

--- stack/stack_lab_2/ques_1.c
+++ stack/stack_lab_2/ques_1.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdbool.h>
 #define max 100
 int top=-1;
 
@@ -8,6 +9,20 @@ char stack[max];
 void push(char element){
     if(top>=max-1){
         printf("\n Overflow condition ! ");
+        return;
     }
     stack[++top] = element;
+}
+
+char pop(){
+    if(top==-1){
+        printf("\n Underflow condition ! ");
+        return '#';
+    }
+    return stack[top--];
+}
+
+
+bool logic(){
+
 }
