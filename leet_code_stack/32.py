cdff30e7376c2b24635840925993250ac2b8b879
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-07-22 07:10:14 +0000

    certain changes made

--- leet_code_stack/32.py
+++ leet_code_stack/32.py
@@ -1,3 +1,17 @@
 class Solution:
     def longestValidParenthesis(self, s:str)->int:
-        
+        max_len = 0
+        left = right = 0
+
+        for char in s:
+            if char=='(':
+                left+=1
+            else:
+                right+=1
+            if left==right:
+                max_len = max(max_len,2*right)
+            elif right>left:
+                left = right = 0
+
+        for char in reversed(s):
+            
