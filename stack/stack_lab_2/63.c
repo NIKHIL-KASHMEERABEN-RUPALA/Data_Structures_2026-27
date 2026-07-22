cc7ed2cb0597d5f312b41299820af1f8b5d81322
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-07-22 03:57:11 +0000

    some changes

---
+++ stack/stack_lab_2/63.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+struct Interval {
+    int start;
+    int end;
+};
+
+struct Interval interval[100];
+struct Interval stack[100];
+
+int top = -1;
+
+void push(struct Interval element){
+    stack[++top] = element;
+}
