902ab49c30b36c7fb4a6b9dcd394eec24d5fb73f
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-07-22 03:48:15 +0000

    made certain changes

---
+++ stack/stack_lab_2/64.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+#define max 20
+
+char stack[max];
+int isVowel(char ch){
+    return (ch=='a' || ch=='A' || ch=='e' || ch == 'E' || ch=='I' || ch=='i' || ch=='o' || ch=='O' || ch == 'u' || ch == 'U');
+}
+
+
+int main(){
+
+
+
+
+
+    return 0;
+}
