a332ac31621ee01c21aced613e04f3d63ccac780
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-07-25 07:18:46 +0000

    made push and pop functions

--- stack/stack_lab_3/66.c
+++ stack/stack_lab_3/66.c
@@ -4,6 +4,23 @@
 #define max 100
 
 char stack[max];
+int top = -1;
+
+void push(char element){
+    if(top==max-1){
+        printf("\n Overflow !");
+        return;
+    }
+    stack[++top] = element;
+}
+
+char pop(){
+    if(top==-1){
+        printf("\n Underflow ! ");
+        return '\0';
+    }
+    return stack[top--];
+}
 
 
 int main(){
