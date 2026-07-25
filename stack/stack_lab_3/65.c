07763f6388bb6291f39c9c7b8640a4ec92531242
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-07-25 04:33:00 +0000

    added some important code

--- stack/stack_lab_3/65.c
+++ stack/stack_lab_3/65.c
@@ -4,8 +4,15 @@
 #define max 100
 
 char stack[max];
+int top = -1;
 
-
+void push(char element){
+    if(top>=max-1){
+        printf("\n Overflow of stack ! ");
+        return;
+    }
+    stack[++top] = element;
+}
 
 
 int main(){
