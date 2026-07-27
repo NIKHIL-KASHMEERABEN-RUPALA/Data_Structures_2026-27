037199fa09e992a6750aa216c90562e6b4ee822e
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-07-27 14:14:30 +0000

    rank function returns 1 for alphabatic sequence

--- stack/stack_lab_3/65.c
+++ stack/stack_lab_3/65.c
@@ -88,6 +88,7 @@ int G(char symbol){
 int R(char symbol){
     if(isalnum(symbol)){
         return 1;
+        
     }
     else if(symbol == '+' || symbol == '-' ||
         symbol == '*' || symbol == '/' ||
