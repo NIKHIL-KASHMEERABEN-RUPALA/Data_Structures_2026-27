67a33a28a1c55fd514a731ba291e250332dab5a7
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-08-02 06:50:17 +0000

    done with lab number 17

--- Lab-17/72.c
+++ Lab-17/72.c
@@ -1,48 +1,87 @@
-#include<stdio.h>
-int queue[5];
+#include <stdio.h>
 
+int queue[5];
 int front = -1;
 int rear = -1;
 
-void Cenqueue(int x){
-    if((rear+1)%5==front){
-        printf("\n Overflow !");
+void Cenqueue(int x) {
+    if ((rear + 1) % 5 == front) {
+        printf("\nOverflow!\n");
         return;
     }
-    rear = (rear+1)%5;
+    rear = (rear + 1) % 5;
     queue[rear] = x;
-    if(front==-1){
+    if (front == -1) {
         front = 0;
     }
+    printf("\nInserted %d successfully.\n", x);
 }
 
-int Cdequeue(){
-    if(front==-1){
-        printf("\n Cqueue underflow ! ");
-        return;
+int Cdequeue() {
+    if (front == -1) {
+        printf("\nCqueue underflow!\n");
+        return -1;
     }
     int y = queue[front];
-    if(front==rear){
-        front = rear=-1;
-    }
-    else{
-        front = (front+1)%5;
+    if (front == rear) {
+        front = rear = -1;
+    } else {
+        front = (front + 1) % 5;
     }
     return y;
 }
 
-void display(){
-    if(front==-1){
-        printf("\n Cqueue is empty ! ");
+void display() {
+    if (front == -1) {
+        printf("\nCqueue is empty!\n");
         return;
     }
 
+    printf("\nQueue elements: ");
     int i = front;
-    do{
-        printf(" %d ",queue[i]);
-        if(i==rear){
+    do {
+        printf("%d ", queue[i]);
+        if (i == rear) {
             break;
         }
-        i = (i+1)%5;
-    }while(i!=rear);
+        i = (i + 1) % 5;
+    } while (i != rear);
+    printf("\n");
+}
+
+int main() {
+    int choice, value;
+
+    while (1) {
+        printf("\n--- Circular Queue Menu ---\n");
+        printf("1. Enqueue\n");
+        printf("2. Dequeue\n");
+        printf("3. Display\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to insert: ");
+                scanf("%d", &value);
+                Cenqueue(value);
+                break;
+            case 2:
+                value = Cdequeue();
+                if (value != -1) {
+                    printf("Dequeued element: %d\n", value);
+                }
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                return 0;
+            default:
+                printf("\nInvalid choice! Please try again.\n");
+        }
+    }
+
+    return 0;
 }
