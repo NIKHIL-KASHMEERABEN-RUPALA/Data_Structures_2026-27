52eb76a241e7e231c94ebef9df6fc6606a8dd07c
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-08-04 12:45:03 +0000

    made 73.c more easy

--- Lab_18/73.c
+++ Lab_18/73.c
@@ -6,143 +6,150 @@ int deque[MAX];
 int front = -1;
 int rear = -1;
 
+// Front end thi element insert karva mate function
 void insertFront(int x)
 {
-    if (front == 0)
+    // Overflow Check: Circular queue property validation
+    if ((front == 0 && rear == MAX - 1) || (front == rear + 1))
     {
         printf("Deque Overflow\n");
+        return;
     }
 
+    // Pehlo element insert thaye tyaare banne pointers 0 par set karo
+    if (front == -1 && rear == -1)
+    {
+        front = rear = 0;
+    }
+    // Circular wrap-around mate logic: front array na end par shift thashe
+    else if (front == 0)
+    {
+        front = MAX - 1;
+    }
     else
     {
-        if (front == -1 && rear == -1)
-        {
-            front = rear = 0;
-        }
-
-        else
-        {
-            front--;
-        }
-
-        deque[front] = x;
+        front--;
     }
+
+    deque[front] = x;
+    printf("%d inserted at Front successfully.\n", x);
 }
 
+// Rear end thi element insert karva mate function
 void insertRear(int x)
 {
-
-    if ((rear + 1) % MAX == front)
+    // Overflow Check
+    if ((front == 0 && rear == MAX - 1) || (front == rear + 1))
     {
-
-        printf("Queue Overflow\n");
+        printf("Deque Overflow\n");
+        return;
     }
 
-    else
-    {
-
-        if (front == -1)
-        {
-
-            front = 0;
-            rear = 0;
-        }
-
-        else
-        {
-
-            rear = (rear + 1) % MAX;
-        }
-
-        deque[rear] = x;
-    }
-}
-
-void deleteFront()
-{
+    // Initial insertion state
     if (front == -1)
     {
+        front = 0;
+        rear = 0;
+    }
+    else
+    {
+        rear = (rear + 1) % MAX;
+    }
 
-        printf("Queue Underflow\n");
+    deque[rear] = x;
+    printf("%d inserted at Rear successfully.\n", x);
+}
 
+// Front end thi element delete karva mate function
+int deleteFront()
+{
+    // Underflow Check
+    if (front == -1)
+    {
+        printf("Deque Underflow\n");
         return -1;
     }
 
     int data = deque[front];
 
+    // Jo ek j element vadhyo hoy ane delete thay toh resets pointers
     if (front == rear)
     {
-
         front = -1;
         rear = -1;
     }
-
     else
     {
-
         front = (front + 1) % MAX;
     }
 
+    printf("%d deleted from Front.\n", data);
     return data;
 }
 
-void deleteRear()
+// Rear end thi element delete karva mate function
+int deleteRear()
 {
+    // Underflow Check
     if (rear == -1)
     {
         printf("Deque Underflow\n");
-
         return -1;
     }
 
     int data = deque[rear];
 
+    // Single element deletion reset
     if (front == rear)
     {
         front = -1;
         rear = -1;
     }
-
+    // Circular wrap-around condition
+    else if (rear == 0)
+    {
+        rear = MAX - 1;
+    }
     else
     {
         rear--;
     }
 
+    printf("%d deleted from Rear.\n", data);
     return data;
 }
 
-// Display
+// Circular Array traversal display logic
 void display()
 {
-    int i;
-
     if (front == -1)
     {
         printf("Deque is Empty\n");
+        return;
     }
 
-    else
+    printf("Deque Elements: ");
+    int i = front;
+
+    // Circular iteration traverse logic
+    while (1)
     {
-        printf("Deque Elements: ");
-
-        for (i = front; i <= rear; i++)
-        {
-            printf("%d ", deque[i]);
-        }
-
-        printf("\n");
+        printf("%d ", deque[i]);
+        if (i == rear)
+            break;
+        i = (i + 1) % MAX;
     }
+
+    printf("\n");
 }
 
-// Main Function
-void main()
+int main()
 {
     int choice, x;
 
     do
     {
         printf("\n----- DOUBLE ENDED QUEUE -----\n");
-
         printf("1. Insert at Front\n");
         printf("2. Insert at Rear\n");
         printf("3. Delete from Front\n");
@@ -181,10 +188,12 @@ void main()
 
         case 6:
             printf("Program Ended\n");
-            return;
+            break;
 
         default:
             printf("Invalid Choice\n");
         }
     } while (choice != 6);
+
+    return 0;
 }
