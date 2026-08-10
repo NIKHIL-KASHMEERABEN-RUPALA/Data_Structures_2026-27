85dcb84eb40f02fd4298fc91c3f6ce5c3c54ae75
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-08-10 11:14:58 +0000

    refactored the code

--- Lab_19/75.c
+++ Lab_19/75.c
@@ -232,8 +232,7 @@ void RINORDER(struct Node *T)
 
     // Traversal Order:
     //     Left Subtree -> Right Subtree -> Root
-
-    
+  
 void RPOSTORDER(struct Node *T)
 {
     // Empty tree check 
