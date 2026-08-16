c02e91a8a265b334fc24beceeb3940d7a3261b1d
NIKHIL RUPALA <rupalanikhil8@gmail.com>
2026-08-16 21:35:34 +0530

    Time: 397 ms (61.25%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,10 +1,20 @@
 # Write your MySQL query statement below
-select 
-    email as Email
-from 
-    Person 
-group by 
-    email
-having 
-    count(email) > 1
+-- select 
+--     email as Email
+-- from 
+--     Person 
+-- group by 
+--     email
+-- having 
+--     count(email) > 1
 
+
+select Distinct
+    p.email as Email 
+from 
+    Person p
+inner join 
+    Person d
+on 
+    p.email = d.email
+    and p.id != d.id
