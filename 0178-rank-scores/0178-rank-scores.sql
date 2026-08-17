928be4203caaad93daa253cc84d52e6fe9c41a66
NIKHIL RUPALA <rupalanikhil8@gmail.com>
2026-08-17 07:50:28 +0530

    Time: 300 ms (90.58%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
 SELECT 
     score,
-    DENSE_RANK() OVER (ORDER BY score DESC) AS `rank` -- i used backticks because rank is a reserved keyword inside MYSQL
-    -- use of dense rank is that ties share the same rank while the every row is numbered without skipping any numbers 
+    DENSE_RANK() OVER (ORDER BY score DESC) AS `rank`
 FROM 
     Scores;
