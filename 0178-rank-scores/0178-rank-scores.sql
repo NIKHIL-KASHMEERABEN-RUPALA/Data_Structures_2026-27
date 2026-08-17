dd85a7c832efbd5ce286406a628037ad29b662fc
NIKHIL RUPALA <rupalanikhil8@gmail.com>
2026-08-17 07:50:07 +0530

    Time: 359 ms (40.98%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -2,5 +2,6 @@
 SELECT 
     score,
     DENSE_RANK() OVER (ORDER BY score DESC) AS `rank` -- i used backticks because rank is a reserved keyword inside MYSQL
+    -- use of dense rank is that ties share the same rank while the every row is numbered without skipping any numbers 
 FROM 
     Scores;
