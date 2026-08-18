dd08b5374e88fbf18b525ceeaff6c34ae84cfea1
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-08-18 17:53:51 +0000

    explained code in gujarati language so that my fellow lab students can understand it easily

--- Lab_13/58.c
+++ Lab_13/58.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Stack Node mate struct ni ghoshana (Structure Definition)
+// Stack Node mate struct nu declaration (Structure Definition)
 struct Node
 {
     int data;
