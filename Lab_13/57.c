06e9e849912ea358d69251063f7fae384322a91e
NIKHIL KASHMEERABEN RUPALA <rupalanikhil8@gmail.com>
2026-08-21 12:01:17 +0000

    made certain code related changes

--- Lab_13/57.c
+++ Lab_13/57.c
@@ -21,7 +21,6 @@ void push()
     printf("Enter value to PUSH: ");
     scanf("%d", &value);
     
-
     // Index ne badhavo ane value ne top position par assign karo
     top++;
     stack[top] = value;
